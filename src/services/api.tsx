937108e01b47f22a808b5687ba6a5167404038ea
Fulal-kt <muhamedfulail77@gmail.com>
2024-01-13 17:03:30 +0530

    loading state in detailspage

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,28 +1,4 @@
-// import axios from "axios"
-// import toast from "react-hot-toast"
-
-
-// const Api=axios.create({
-//     baseURL:import.meta.env.VITE_BASEURL
-// })
-
-
-
-// if (localStorage.getItem("token")) {
-//     Api.interceptors.request.use((request) => {
-//         request.headers.Authorization = localStorage.getItem("token")
-//         return request
-//     })
-// }
-
-// Api.interceptors.response.use(response => response, err => {
-//     if(err.response && err.response.status === 500){
-//         toast.error("internal server error")
-//     }
-//     return Promise.reject(err)
-// })
-
-// export default Api
+ 
 
 import axios from "axios";
 import toast from "react-hot-toast";
